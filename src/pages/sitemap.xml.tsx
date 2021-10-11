d0f89a1db902d1adf9bf8b73eca6fa1247a27f36
carlogino <carlogino@ntucenterprise.sg>
2021-10-11 14:56:50 +0800

    feat(blog): Created dynamic sitema for blogs

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,112 @@
+import fs from 'fs';
+import {GetServerSideProps} from 'next';
+import path from 'path';
+import {BlogMetadata} from '../models/blog';
+
+type SiteMapUrl = {
+  slug: string;
+  lastMod: string;
+  priority: number;
+  changeFrequency:
+    | 'never'
+    | 'yearly'
+    | 'monthly'
+    | 'weekly'
+    | 'daily'
+    | 'hourly'
+    | 'always';
+};
+
+const Sitemap = () => {};
+
+const staticPages: SiteMapUrl[] = [
+  {
+    slug: '',
+    lastMod: '2021-10-11',
+    priority: 1.0,
+    changeFrequency: 'monthly',
+  },
+  {
+    slug: '/about',
+    lastMod: '2021-10-11',
+    priority: 1.0,
+    changeFrequency: 'monthly',
+  },
+  {
+    slug: '/resume',
+    lastMod: '2021-10-11',
+    priority: 1.0,
+    changeFrequency: 'monthly',
+  },
+  {
+    slug: '/blog',
+    lastMod: '2021-10-11',
+    priority: 1.0,
+    changeFrequency: 'monthly',
+  },
+];
+
+const generateBlogSiteMapData = () => {
+  const postsDirectory = path.join(process.cwd(), 'src/pages/blog');
+  const blogDirectories = fs
+    .readdirSync(postsDirectory)
+    .filter(filename => !filename.includes('index.tsx')); // exclude this file
+
+  const blogsSitemaps: SiteMapUrl[] = blogDirectories.map(directory => {
+    const mdxContent = require(`./blog/${directory}/index.mdx`);
+
+    const meta: BlogMetadata = mdxContent?.meta ?? {};
+
+    return {
+      slug: directory,
+      changeFrequency: 'monthly',
+      lastMod: meta.date,
+      priority: 1,
+    };
+  });
+
+  return blogsSitemaps;
+};
+
+const ROOT_URL = 'https://www.carlogino.cc';
+
+export const getServerSideProps: GetServerSideProps = async ({res}) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map(sitemap => {
+          return `
+            <url>
+              <loc>${ROOT_URL}${sitemap.slug}</loc>
+              <lastmod>${sitemap.lastMod}</lastmod>
+              <changefreq>${sitemap.changeFrequency}</changefreq>
+              <priority>${sitemap.priority}</priority>
+            </url>
+          `;
+        })
+        .join('')}
+      ${generateBlogSiteMapData()
+        .map(sitemap => {
+          return `
+            <url>
+              <loc>${ROOT_URL}/blog/${sitemap.slug}}</loc>
+              <lastmod>${sitemap.lastMod}</lastmod>
+              <changefreq>${sitemap.changeFrequency}</changefreq>
+              <priority>${sitemap.priority}</priority>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>
+  `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
