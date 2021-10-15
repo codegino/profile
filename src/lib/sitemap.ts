5220b1f12d061d479b704e2ceb6220c0b6a25b45
carlogino <carlogino@ntucenterprise.sg>
2021-10-15 08:39:36 +0800

    Updated sitemap

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -70,13 +70,14 @@ const ROOT_URL = 'https://carlogino.cc';
 async function generateSitemap() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <url>
+    <loc>https://www.carlogino.cc</loc>
+  </url>
   ${staticPages
     .map(sitemap => {
       return `
         <url>
           <loc>${ROOT_URL}${sitemap.slug}</loc>
-          <lastmod>${sitemap.lastMod}</lastmod>
-          <changefreq>${sitemap.changeFrequency}</changefreq>
           <priority>${sitemap.priority}</priority>
         </url>
       `;
