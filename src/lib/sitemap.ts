73d147670f838dafe9cacbb33777df7558be1c23
Carlo Gino Catapang <carloginocatapang@gmail.com>
2021-10-15 23:55:07 +0800

    Update sitemap.ts

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -70,13 +70,13 @@ const generateBlogSiteMapData = (): SiteMapUrl[] => {
   return blogsSitemaps;
 };
 
-const ROOT_URL = 'https://carlogino.cc';
+const ROOT_URL = 'https://www.carlogino.cc';
 
 async function generateSitemap() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
-    <loc>https://www.carlogino.cc</loc>
+    <loc>https://carlogino.cc</loc>
   </url>
   ${staticPages
     .map(sitemap => {
