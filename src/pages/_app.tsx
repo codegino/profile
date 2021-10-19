0c10d933c14bcff7ecc965101b0f83712fe0a5a5
carlogino <carlogino@ntucenterprise.sg>
2021-10-19 18:13:29 +0800

    chore(perf): Apply analytics only during production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,18 +10,20 @@ function MyApp({Component, pageProps}: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange);
+    if (process.env.NODE_ENV === 'production') {
+      const handleRouteChange = (url: string) => {
+        ga.pageview(url);
+      };
+      //When the component is mounted, subscribe to router changes
+      //and log those page views
+      router.events.on('routeChangeComplete', handleRouteChange);
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
+      // If the component is unmounted, unsubscribe
+      // from the event with the `off` method
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    }
   }, [router.events]);
 
   return (
