8a401bdbc2ef4020f7a2914abb0af161b2eb2358
Carlo Gino Catapang <carloginocatapang@gmail.com>
2021-11-16 18:59:24 +0800

    Update SocialMedia.tsx

--- src/components/social/SocialMedia.tsx
+++ src/components/social/SocialMedia.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import {FaFacebookSquare} from '@react-icons/all-files/fa/FaFacebookSquare';
+import {FaDev} from '@react-icons/all-files/fa/FaDev';
 import {FaGithubSquare} from '@react-icons/all-files/fa/FaGithubSquare';
 import {FaLinkedin} from '@react-icons/all-files/fa/FaLinkedin';
 import {FaTwitterSquare} from '@react-icons/all-files/fa/FaTwitterSquare';
