858a113cca0321e985ccdf78c5567078f26ea554
codegino <carloginocatapang@gmail.com>
2021-12-04 18:01:23 +0800

    feat: Added newsletter landing page

---
+++ src/pages/signup-success.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import Head from 'next/head';
+
+export default function Blog() {
+  return (
+    <>
+      <Head>
+        <title>Thank you for signing up</title>
+      </Head>
+      <Container>
+        <Content>
+          <h1>Success🥳</h1>
+          <h1>Welcome aboard to my newsletter! 📬</h1>
+          <h2>
+            To ensure you will receive the latest and greatest updates, please
+            confirm your subscription by clicking the link I send in your
+            email.📧
+          </h2>
+
+          <br />
+          <p>Thank you for signing up.🙏</p>
+          <p>We&lsquo;ll be in touch soon!👋</p>
+        </Content>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.main`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  min-height: 80vh;
+  padding: var(--spacing-medium) 0;
+`;
+
+const Content = styled.article`
+  text-align: center;
+  max-width: 50rem;
+`;
