f0be1ec1262ee5d297f35a9d017c6c45322bd830
codegino <carloginocatapang@gmail.com>
2021-12-08 18:17:58 +0800

    chore(seo): Updated position of meta tags

--- src/frontend-utils/meta-tags.tsx
+++ src/frontend-utils/meta-tags.tsx
@@ -17,5 +17,33 @@ export const commonMetaTags = (slug = '') => (
     <meta property="og:image" content="/assets/preview.png" />
 
     <meta name="description" content={metaDescription} />
+
+    <link rel="icon" href="/favicon.ico" />
+
+    {/* For PWA */}
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="apple-touch-icon" href="/assets/logo.png"></link>
+
+    {/* ABtesting.ai Code */}
+    <link rel="preconnect" href="https://external.abtesting.ai" />
+    <script async src="https://js.abtesting.ai/ab.js?userid=5393" />
+
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />
   </>
 );
