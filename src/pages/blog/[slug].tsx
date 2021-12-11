fc1892d2d0af03fd4b41bd784416c5c05bbd601c
codegino <carloginocatapang@gmail.com>
2021-12-11 18:50:05 +0800

    feat(blog): created Screpy review

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -72,7 +72,7 @@ export default function BlogPage({
         <main>
           <BlogContent source={source} />
         </main>
-        <hr />
+        <br />
         <BlogFooter blog={blog} />
       </BlogLayout>
     </>
