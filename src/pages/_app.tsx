dd3b4f797098cb3f2a808060c4af1469565afdab
codegino <carloginocatapang@gmail.com>
2022-01-02 09:14:42 +0800

    fix: removed manual GA tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import useDarkMode from 'use-dark-mode';
 import '../../styles/tailwind.css';
 import Footer from '../components/Footer';
 import Header from '../components/header/Header';
-import * as ga from '../lib/ga';
 import '../styles/_globals.css';
 import '../styles/animations.css';
 import '../styles/custom.css';
@@ -23,23 +22,6 @@ function MyApp({Component, pageProps}: AppProps) {
       document.documentElement.classList.remove('dark');
     }
   }, [isDarkMode]);
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      const handleRouteChange = (url: string) => {
-        ga.pageview(url);
-      };
-      //When the component is mounted, subscribe to router changes
-      //and log those page views
-      router.events.on('routeChangeComplete', handleRouteChange);
-
-      // If the component is unmounted, unsubscribe
-      // from the event with the `off` method
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange);
-      };
-    }
-  }, [router.events]);
 
   return (
     <>
