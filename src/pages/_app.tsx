143f65a52f917d86f9656f0506ca007435ae4fa6
codegino <carloginocatapang@gmail.com>
2022-01-02 12:52:04 +0800

    feat: replaced all trackers with gtm module

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import {useEffect} from 'react';
 import type {AppProps} from 'next/app';
-import {useRouter} from 'next/router';
-import Script from 'next/script';
+import TagManager from 'react-gtm-module';
 import useDarkMode from 'use-dark-mode';
 import '../../styles/tailwind.css';
 import Footer from '../components/Footer';
@@ -11,8 +10,6 @@ import '../styles/animations.css';
 import '../styles/custom.css';
 
 function MyApp({Component, pageProps}: AppProps) {
-  const router = useRouter();
-
   const {value: isDarkMode} = useDarkMode();
 
   useEffect(() => {
@@ -23,27 +20,15 @@ function MyApp({Component, pageProps}: AppProps) {
     }
   }, [isDarkMode]);
 
+  // Google Tag Manager
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      TagManager.initialize({gtmId: process.env.NEXT_PUBLIC_GTM as string});
+    }
+  }, []);
+
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Header />
       <Component {...pageProps} />
       <Footer />
