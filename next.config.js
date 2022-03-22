09efa806a10780c2bc2170ce4d8c8dfd54386302
codegino <carloginocatapang@gmail.com>
2022-03-22 09:36:53 +0800

    chore: added dev.to s3 for image loading

--- next.config.js
+++ next.config.js
@@ -10,7 +10,12 @@ const config = {
   pageExtensions: ['js', 'ts', 'jsx', 'tsx', 'md', 'mdx'],
   compress: true,
   images: {
-    domains: ['i.imgur.com', 'media2.giphy.com', 'images.ctfassets.net'],
+    domains: [
+      'i.imgur.com',
+      'media2.giphy.com',
+      'images.ctfassets.net',
+      'dev-to-uploads.s3.amazonaws.com',
+    ],
     imageSizes: [16, 32, 48, 64],
     deviceSizes: [
       96, 128, 256, 384, 512, 640, 750, 828, 1080, 1200, 1920, 2048, 3840,
