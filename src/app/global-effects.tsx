72b33e07dc3443823f29337096241c928a84c662
codegino <carloginocatapang@gmail.com>
2023-06-27 19:49:07 +0200

    feat: Added common effects

---
+++ src/app/global-effects.tsx
@@ -0,0 +1,37 @@
+'use client';
+import Script from 'next/script';
+import React, {useEffect} from 'react';
+import TagManager from 'react-gtm-module';
+import useDarkMode from 'use-dark-mode';
+
+const GlobalEffects = () => {
+  const {value: isDarkMode} = useDarkMode();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  // Google Tag Manager
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      TagManager.initialize({gtmId: process.env.NEXT_PUBLIC_GTM as string});
+    }
+  }, []);
+  return (
+    <>
+      <Script
+        id="birdeatsbug-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(){const birdeatsbug=(window.birdeatsbug=window.birdeatsbug||[]);if(birdeatsbug.initialize)return;if(birdeatsbug.invoked){if(window.console&&console.error){console.error('birdeatsbug snippet included twice.')}return}birdeatsbug.invoked=true;birdeatsbug.methods=['setOptions','startSession','resumeSession','startRecording','stopRecording','stopSession','uploadSession','deleteSession'];birdeatsbug.factory=function(method){return function(){var args=Array.prototype.slice.call(arguments);args.unshift(method);birdeatsbug.push(args);return birdeatsbug}};for(var i=0;i<birdeatsbug.methods.length;i++){var key=birdeatsbug.methods[i];birdeatsbug[key]=birdeatsbug.factory(key)}birdeatsbug.load=function(){var script=document.createElement('script');script.type='module';script.async=true;script.src='https://sdk.birdeatsbug.com/latest/core.js';var first=document.getElementsByTagName('script')[0];first.parentNode.insertBefore(script,first)};birdeatsbug.load();window.birdeatsbug.setOptions({publicAppId:'51c64882-9518-40f4-ad71-4975cc5bf3de'})})();`,
+        }}
+      ></Script>
+    </>
+  );
+};
+
+export default GlobalEffects;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import Head from 'next/head';
+import GlobalEffects from './global-effects';
+
 export const metadata = {
   title: 'TODO: // Update this title',
   description: 'TODO: // Update this description',
@@ -6,7 +9,36 @@ export const metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content={`${process.env.NEXT_PUBLIC_GOOOGLE_SITE_VERIFICATION}`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@codegino" />
+        <meta name="twitter:site" content="@codegino" />
+        <meta name="author" content="Carlo Gino Catapang" />
+        <meta
+          name="keywords"
+          content="React,React Native,Javascript,Typescript,Remix,NodeJS,HTML,CSS"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="white"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="black"
+        />
+        <link
+          rel="stylesheet"
+          href="https://sdk.birdeatsbug.com/latest/style.css"
+        ></link>
+      </Head>
       <body>{children}</body>
+      <GlobalEffects />
     </html>
   );
 }
