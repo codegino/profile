19fc675d3bd8385c03825895008c4761624ce719
codegino <carloginocatapang@gmail.com>
2023-12-09 19:51:30 +0100

    publish new blog

--- src/modules/common/ContentLayout.tsx
+++ src/modules/common/ContentLayout.tsx
@@ -13,13 +13,14 @@ const ContentLayout: FunctionComponent<{children: React.ReactNode}> = ({
         'm-auto px-2 mb-10 flex flex-col max-w-screen-md',
         '[&_*_code]:bg-neutral-700 [&_*_code]:dark:bg-neutral-200 [&_*_code]:text-neutral-200 [&_*_code]:dark:text-neutral-700',
         '[&>pre]:my-3',
-        '[&_blockquote>p]:pl-3',
+        '[&_blockquote>p]:pl-2',
         '[&>ul]:m-0',
         '[&_:is(ol,ul)>li]:ml-8 [&:is(ol,ul)>li]:text-base [&:is(ol,ul)>li]:leading-5',
         '[&>ol>li]:list-decimal [&>ul>li]:list-disc',
-        '[&>ol]:m-0 [&>p]:px-3 [&>p]:mx-3',
+        '[&>ol]:m-0 [&>p]:px-2 [&>p]:mx-2',
         '[&_:is(li,p,h2,h3,h4,h5,h6)_code]:whitespace-nowrap [&_:is(li,p,h2,h3,h4,h5,h6)_code]:px-1 [&_:is(li,p,h2,h3,h4,h5,h6)_code]:rounded-[3px] [&_:is(li,p,h2,h3,h4,h5,h6)_code]:font-roboto [&_:is(li,p,h2,h3,h4,h5,h6)_code]:text-[0.95rem]',
         '[&_:is(h2,h3,h4)_em]:text-sm',
+        '[&_h4]:mt-4',
       )}
     >
       {children}
