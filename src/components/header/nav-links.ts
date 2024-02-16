943dd6faecaf94273aed5e9da58a4393c1a440b5
codegino <carloginocatapang@gmail.com>
2024-02-16 15:00:58 +0100

    Remove novel link from navigation

--- src/components/header/nav-links.ts
+++ src/components/header/nav-links.ts
@@ -3,7 +3,6 @@ import type {AnchorHTMLAttributes} from 'react';
 export const navigationLinks: NavLink[] = [
   {url: '/resume', label: 'resume'},
   {url: '/about', label: 'about'},
-  {url: '/novel', label: 'novel'},
   {url: '/nft', label: 'nft'},
   {url: 'https://notes.carlogino.com', label: 'Notes'},
   {url: 'https://gpt.carlogino.com', label: 'Chatbot', target: '_blank'},
