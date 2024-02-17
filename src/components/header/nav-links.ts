a2b286360cad7bed14d5b3a9b866d2a5c2870481
codegino <carloginocatapang@gmail.com>
2024-02-17 09:57:55 +0100

    Update Notes URL in navigationLinks

--- src/components/header/nav-links.ts
+++ src/components/header/nav-links.ts
@@ -4,7 +4,7 @@ export const navigationLinks: NavLink[] = [
   {url: '/resume', label: 'resume'},
   {url: '/about', label: 'about'},
   {url: '/nft', label: 'nft'},
-  {url: 'https://notes.carlogino.com', label: 'Notes'},
+  {url: 'https://note.carlogino.com', label: 'Notes'},
   {url: 'https://gpt.carlogino.com', label: 'Chatbot', target: '_blank'},
 ];
 
