2d52ca19ebde75b752474d54078afdfebcac3e91
codegino <carloginocatapang@gmail.com>
2025-10-04 17:23:03 +0200

    remove contentful

--- src/utils/contentful.utils.ts
+++ src/utils/contentful.utils.ts
@@ -1,11 +1,6 @@
-import { createClient } from 'contentful';
 import { blurImage } from './image-blur.utils';
 import { getImageUrl } from './get-image';
 
-export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
-});
 
 export async function getBlurringImage(key: string, prefix = "static") {
   const url = getImageUrl(key, prefix)
